3d6b4b4cd09a7d649f528096396715c43aaa7772
timcknowles <tim.c.knowles@gmail.com>
2018-05-26 22:01:30 +0100

    change import statements

--- acoidea/wsgi.py
+++ acoidea/wsgi.py
@@ -7,6 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
+
+from __future__ import absolute_import, unicode_literals
+
 import os
 
 from django.core.wsgi import get_wsgi_application
